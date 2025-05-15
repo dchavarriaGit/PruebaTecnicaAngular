c3886a3f14fca211a62485e86666e31d8ea1aa56
dchavarria <dchavarria@rentingcolombia.com>
2025-05-15 09:58:00 -0500

    "Se logra compilar el proyecto"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  styleUrls: './app.component.html',
+  standalone: true,
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
   imports: [CommonModule, RouterModule]
 })
 export class AppComponent implements OnInit {
   title = '¡Bienvenido(a)!';
+  
 
   constructor(private http: HttpClient) {}
 
